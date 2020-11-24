80d47536d36ac5d3657213842282554905ac0369
franknfjr <frankdonascimentojr@gmail.com>
2020-11-23 23:52:08 -0300

    function

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const name = "Frank",
+    age = 27,
+    gender = "Male";
+
+const sayHi = (name, age, gender ) => {
+    console.log(`Hi ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(name, age, gender);
+export {}
