55d29223360085a74758ec4cf07baccf8800c1e6
franknfjr <frankdonascimentojr@gmail.com>
2020-11-29 21:08:29 -0300

    interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string):  string => {
-    return `Hi ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+    name: string,
+    age: number,
+    gender: string
+}
+
+const person = {
+    name: "Frank",
+    age: 26,
+    gender: "male"
+}
+
+const sayHi = (person: Human): string => {
+    return `Hi ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi("Frank", 26, "Male"));
+console.log(sayHi(person));
 
-export {}
+export { }
