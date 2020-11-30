57d08f16554a91a01535a7eb8ec53b8b1d06b804
franknfjr <frankdonascimentojr@gmail.com>
2020-11-29 21:19:51 -0300

    class

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
-interface Human {
-    name: string,
-    age: number,
-    gender: string
+// interface Human {
+//     name: string,
+//     age: number,
+//     gender: string
+// }
+
+// const person = {
+//     name: "Frank",
+//     age: 26,
+//     gender: "male"
+// }
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "Frank",
-    age: 26,
-    gender: "male"
-}
+const frank = new Human("Frank", 26, "male")
 
 const sayHi = (person: Human): string => {
     return `Hi ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(frank));
 
 export { }
